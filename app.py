05081bcb6fef370f0956b6bfcfac6c22b1926bbc
VictorAID <victorboladejamb17@gmail.com>
2024-05-02 20:02:08 +0100

    I tested the function using a value 14

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import math
-
 def calculate_sphere_volume(radius):
   volume = (4/3) * math.pi * (radius**3)
   return volume
 
 print(calculate_sphere_volume(7))
+print(calculate_sphere_volume(14))
